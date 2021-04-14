f73ae33dd573783a1dcf4a1dc2724547278839ba
AngelLGonzalezViewnext <algonzalezm@viewnext.com>
2021-04-14 11:35:50 +0200

    primer commit

---
+++ src/main/EjercicioEclipseMain.java
@@ -0,0 +1,37 @@
+package main;
+
+
+public class EjercicioEclipseMain {
+	private String testImprimir;
+	
+
+	public EjercicioEclipseMain() {
+		this.testImprimir = "";
+	}
+
+
+	public static void main(String[] args) {
+		EjercicioEclipseMain ejemplo1=new EjercicioEclipseMain();
+		ejemplo1.setTestImprimir("valor xxxx");
+		System.out.println("holaaaa ");
+		System.out.println(ejemplo1);
+
+	}
+
+
+	public String getTestImprimir() {
+		return testImprimir;
+	}
+
+
+	public void setTestImprimir(String numImprimir) {
+		this.testImprimir = numImprimir;
+	}
+
+
+	@Override
+	public String toString() {
+		return "EjercicioEclipseMain [testImprimir=" + testImprimir + "]";
+	}
+
+}
